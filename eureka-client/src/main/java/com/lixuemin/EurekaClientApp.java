23ad4a43032cd1f3515cb224cf1243c48a1c6226
lixuemin <lxmheida@gmail.com>
2022-06-07 21:31:27 +0800

    init

---
+++ eureka-client/src/main/java/com/lixuemin/EurekaClientApp.java
@@ -0,0 +1,20 @@
+package com.lixuemin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class EurekaClientApp {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaClientApp.class).web(true).run(args);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+#### 微服务框架按功能分类
+
+![](http://processon.com/chart_image/629ef4100e3e742555d27268.png)
+
